8b686f448f3e8df29818e32d759201475b6d7918
zoran <zoran@DESKTOP-ZOKI>
2022-08-30 19:33:48 +0200

    Create Startup class

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -6,10 +6,14 @@ namespace eTickets
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var startup = new Startup(builder.Configuration);
+            startup.ConfigurationServices(builder.Services);
+
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            // builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+            startup.Configure(app, builder.Environment);
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -19,18 +23,16 @@ namespace eTickets
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            // app.UseHttpsRedirection();
+            // app.UseStaticFiles();
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            //app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
+            //app.Run();
         }
     }
 }
---
+++ eTickets/Startup.cs
@@ -0,0 +1,43 @@
+﻿using eTickets.Data;
+
+namespace eTickets
+{
+    public class Startup
+    {
+        public IConfiguration Configuration
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigurationServices(IServiceCollection services)
+        {
+            // DbContext configuration
+            services.AddDbContext<AppDbContext>();
+
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllerRoute(
+               name: "default",
+               pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+            app.Run();
+        }
+
+    }
+}
